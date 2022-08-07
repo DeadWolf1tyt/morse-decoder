c42a5e0bcca434e5d52de17d452c094a0dc541df
DeadWolf1tyt <deadwolftyt@gmail.com>
2022-08-07 10:51:47 +0300

    add fiel index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,62 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	let message = []; // массив в катором сообщение разбито на строки по 10 цифр
+	let counts = 10; // счетчик для отщета 10 цифр
+	let indMes = 0; // индекс массива message
+ 
+	for (let i = 0; i < expr.length; i++) {
+		counts -= 1;
+		message[indMes] = message[indMes] + expr[i];
+		if (counts === 1) {  // удаление из массива undefined
+			message[indMes] = message[indMes].slice(9);
+		}
+		if (counts === 0) { // сброс счетчика цифр
+			counts += 10;
+			indMes++;
+		}
+	}
+	let arrMorse = []; //массив строк точки и тире
+ 
+	message.forEach((item, index) => {
+		let arrItem = item.split('');
+		let symbolMorse = '';
+		let symbolMorseI = 0;
+		let pairMorse = '';
+		
+	arrItem.forEach((el, i) => {
+		let pair = '';
+		if (i % 2 === 0) {
+			pair = pair + el + arrItem[i + 1];
+			let Morse = '';
+			switch(pair) {
+				case "00": Morse = Morse + ''; break;
+				case "10": Morse = Morse + '.'; break;
+				case "11": Morse = Morse + '-'; break;
+				case "**": Morse = Morse + '*'; break;
+			}
+			symbolMorse += Morse;
+		}
+		});
+		
+		arrMorse[index] = symbolMorse;
+	});
+ 
+	let messageStr = []; //массив букв
+	arrMorse.forEach((item, i) => {
+		messageStr[i] = MORSE_TABLE[item];
+	});
+ 
+	console.log(messageStr);
+	let strings = '';
+	messageStr.forEach((item, i) => {
+		if (item === undefined) {
+			strings += ' ';
+		} else {
+			strings += messageStr[i];
+		}
+		console.log(strings);
+	});
 }
 
 module.exports = {
